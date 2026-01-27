4853f77c7727f83e8a73adbd730eee9ca6c57e76
HoangThong26 <hbt2k4@gmail.com>
2026-01-28 00:01:41 +0700

    Implement MINUS operation

--- operator/CalculatorService.cs
+++ operator/CalculatorService.cs
@@ -1,33 +1,29 @@
 ﻿using static CalculatorService.SIGN;
 
-<<<<<<< Updated upstream
 namespace CalculatorService
 {
 
 
-=======
 
-namespace CalculatorService
-{
-
->>>>>>> Stashed changes
-    public class CalculatorService
+    namespace CalculatorService
     {
-        private Double calculate(SIGN sign, Double arg1, Double arg2)
-        {
-            switch (sign)
-            {
-                case MINUS:
-<<<<<<< Updated upstream
-                    return arg1 - arg2;
-=======
-                    return arg1 / arg2;
->>>>>>> Stashed changes
-                default:
-                    break;
-            }
-            return 0d;
-        }
 
+
+        public class CalculatorService
+        {
+            private Double calculate(SIGN sign, Double arg1, Double arg2)
+            {
+                switch (sign)
+                {
+                    case MINUS:
+                        return arg1 - arg2;
+                    default:
+                        break;
+                }
+                return 0d;
+            }
+
+
+        }
     }
 }
