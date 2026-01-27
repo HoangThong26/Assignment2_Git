a18fe618a530008e27de2608f11b86ce0373897f
HoangThong26 <hbt2k4@gmail.com>
2026-01-27 23:46:36 +0700

    Implement MINUS operation

---
+++ operator/SIGN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorService
+{
+    public enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+}
