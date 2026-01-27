c22caa30a30dcc2d0c0e20563c6977f47a94d435
HoangThong26 <hbt2k4@gmail.com>
2026-01-27 23:50:05 +0700

    Implement MINUS operation

--- operator/obj/Debug/net10.0/operator.AssemblyInfo.cs
+++ operator/obj/Debug/net10.0/operator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("operator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeaa276ed8aad1250eefa73b0c071a5ae215af6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18fe618a530008e27de2608f11b86ce0373897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("operator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("operator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
