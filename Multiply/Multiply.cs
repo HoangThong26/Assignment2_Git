0c4ce8735c606610fe0d751f78c84ad5de9a9d7f
HoangThong26 <hbt2k4@gmail.com>
2026-01-28 21:12:31 +0700

    Implement Multiply

---
+++ Multiply/Multiply.cs
@@ -0,0 +1,10 @@
+double Calculate(double a, double b, char op)
+{
+    switch (op)
+    {
+        case '*':
+            return a * b;
+        default:
+            throw new ArgumentException("Invalid operator");
+    }
+}
