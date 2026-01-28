4a21e9f5beca69abc70e3536b9a6e2d5edb55ae6
HoangThong26 <hbt2k4@gmail.com>
2026-01-28 14:13:54 +0700

    Implement Multiply

---
+++ Multiply/Program.cs
@@ -0,0 +1,19 @@
+﻿using static Multiply.SIGN;
+
+namespace Multiply
+{
+    internal class Program
+    {
+        private double calculate(SIGN sign, double arg1, double arg2)
+        {
+            switch (sign)
+            {
+                case MULTIPLY:
+                    return arg1 * arg2;
+                default:
+                    break;
+            }
+            return 0d;
+        }
+    }
+}
