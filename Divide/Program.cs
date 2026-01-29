c0423ce5b1fa40d6afb20e4cfb5c3d72191f3f6b
LaHiep2004 <lahiep2k4@gmail.com>
2026-01-29 09:29:10 +0700

    Add Divide

---
+++ Divide/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Divide
+{
+        public enum SIGN
+        {
+            PLUS,
+            MINUS,
+            MULTIPLY,
+            DIVIDE
+        }
+        public class Program
+        {
+            private Double calculate(SIGN sign, Double arg1, Double arg2)
+            {
+                switch (sign)
+                {
+                    case SIGN.DIVIDE:
+                    if (arg2 == 0) throw new DivideByZeroException();
+                        return arg1/ arg2;
+                    default:
+                        break;
+                }
+                return 0d;
+            }
+        }
+    }
--- phepnhan/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿public class phepnhan
-{
-    public int Multiply(int a, int b)
-    {
-        return a * b;
-    }
-}
