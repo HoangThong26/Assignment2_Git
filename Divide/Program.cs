bcc8f2e84774f304606b84481ff3a17fd259fe5e
tunanhh9 <ngtuananh9604@gmail.com>
2026-01-29 10:41:54 +0700

    Divide

---
+++ Divide/Program.cs
@@ -0,0 +1,12 @@
+﻿double Calculate(double a, double b, char op)
+{
+    switch (op)
+    {
+        case '/':
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero");
+            return a / b;
+        default:
+            throw new ArgumentException("Invalid operator");
+    }
+}
